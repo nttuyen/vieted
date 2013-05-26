5236f5dc7165640a04d6746babcaef26661ea39d
thi nguyen phong <potter.nguyen@gmail.com>
2013-05-26 10:21:36 +0700

    thinp

---
+++ src/com/vieted/android/app/activity/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.vieted.android.app.activity;
+
+import android.os.Bundle;
+
+import com.androidteam.base.task.RestAsyncTask;
+import com.vieted.android.app.R;
+
+public class RegisterActivity extends VietEdBaseActivity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setTextHeader("Register");
+		this.setLayoutBody(R.layout.activity_register_account);
+	}
+	
+	@Override
+	protected void handleGetSuccess(RestAsyncTask task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
